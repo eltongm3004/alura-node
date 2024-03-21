fa9f3b74167204cb98c4014090403ae50fc3bda4
Elton G Mitiura <eltongm@hotmail.com>
2024-03-20 22:47:00 -0300

    Corrigido a conexão com mongodb

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://eltongm:123@cluster0.tuilkkn.mongodb.net/alura-node");
+mongoose.connect("mongodb+srv://eltongm:123@alura.z3e8un7.mongodb.net/alura-node");
 
 let db = mongoose.connection;
 
